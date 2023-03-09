d0f65c6d0a734bda47e8773aa31bee4db1a5ea7c
JF0HE1 <datastructures@algorithms>
2023-03-09 04:08:50 +0000

    assignment 6 first iteration

---
+++ buildroot/output/.br2-external.mk
@@ -0,0 +1,14 @@
+#
+# Automatically generated file; DO NOT EDIT.
+#
+
+BR2_EXTERNAL ?= /home/moskito/Desktop/Desktop/code/coursera/systems_prog/Ass5.2/assignment-5-moskwitto/base_external
+BR2_EXTERNAL_NAMES = 
+BR2_EXTERNAL_DIRS = 
+BR2_EXTERNAL_MKS = 
+
+BR2_EXTERNAL_NAMES += project_base
+BR2_EXTERNAL_DIRS += /home/moskito/Desktop/Desktop/code/coursera/systems_prog/Ass5.2/assignment-5-moskwitto/base_external
+BR2_EXTERNAL_MKS += /home/moskito/Desktop/Desktop/code/coursera/systems_prog/Ass5.2/assignment-5-moskwitto/base_external/external.mk
+export BR2_EXTERNAL_project_base_PATH = /home/moskito/Desktop/Desktop/code/coursera/systems_prog/Ass5.2/assignment-5-moskwitto/base_external
+export BR2_EXTERNAL_project_base_DESC = project_base
